c80e994816fb838452801c1bbb4991b456551230
John Micko <jrmicko5012@gmail.com>
2020-11-13 02:07:56 -0600

    remove second input Extra operators no longer needed as calculation will be done on one line buttons add numbers and opperators to input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,17 @@
 $(window).on("load", function () {
     console.log('hello from jq');
     // Handler when all assets (including images) are loaded
-
-
+    
+    // add a listeneer to all buttons
+    $('.numpad').on('click', whichButton);
 });
+
+// 
+function whichButton() {
+    let buttonId = $(this).text();
+    console.log(buttonId);
+    $('#calculation-in').val(function() {
+        return this.value + '' + buttonId;
+    }
+    )
+}
