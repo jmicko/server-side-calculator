ffd4ac33a64b6db4a029f3448bd14a567b784a90
John Micko <jrmicko5012@gmail.com>
2020-11-14 16:16:45 -0600

    move organizedArray inside function so not global

--- server/public/scripts/calculator.js
+++ server/public/scripts/calculator.js
@@ -1,15 +1,15 @@
 module.exports = makeCalculation;
 
-// set up an array to hold all our numbers, and our operators in an organized way
-const organizedArray = [];
-// set up a variable to keep track of the position in the array.
-// this will change every time an operator is pressed. Concat new 
-// number button presses to current position, and store operators 
-// in between numbers
-let position = 0;
 
 
 function makeCalculation(input) {
+    // set up an array to hold all our numbers, and our operators in an organized way
+    const organizedArray = [];
+    // set up a variable to keep track of the position in the array.
+    // this will change every time an operator is pressed. Concat new 
+    // number button presses to current position, and store operators 
+    // in between numbers
+    let position = 0;
     // loop through each character of the string taken from the input field
     for (let i = 0; i < input.calcString.length; i++) {
         console.log('we are working with', input.calcString[i]);
